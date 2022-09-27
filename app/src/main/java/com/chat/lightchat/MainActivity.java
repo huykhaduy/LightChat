adf37143682fc248e4c497ab7dd0313004c21d17
huykhaduy <huykhaduy@gmail.com>
2022-09-27 12:28:20 +0700

    First commit

---
+++ app/src/main/java/com/chat/lightchat/MainActivity.java
@@ -0,0 +1,18 @@
+package com.chat.lightchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword("duy123@gmail.com","03123131233");
+    }
+}
