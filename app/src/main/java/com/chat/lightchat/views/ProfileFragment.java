8383887fcf0f5e1f70de298446602bf46f2703c2
trangthanhphuc <trangthanhphuc91@gmail.com>
2022-11-05 02:27:06 +0700

    intent camera ava ProfileFragment

--- app/src/main/java/com/chat/lightchat/views/ProfileFragment.java
+++ app/src/main/java/com/chat/lightchat/views/ProfileFragment.java
@@ -1,7 +1,10 @@
 package com.chat.lightchat.views;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,10 +13,16 @@ import android.view.ViewGroup;
 
 import com.chat.lightchat.R;
 import com.chat.lightchat.databinding.FragmentProfileBinding;
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class ProfileFragment extends Fragment {
 
+    CircleImageView imgAva;
+    FloatingActionButton floatingactionbutton;
 
 
     public ProfileFragment() {
@@ -25,8 +34,35 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        FragmentProfileBinding binding = FragmentProfileBinding.inflate(inflater, container, false);
+//        FragmentProfileBinding binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        return binding.getRoot();
+
+        floatingactionbutton = view.findViewById(R.id.floatingActionButton);
+        imgAva = view.findViewById(R.id.imgProject);
+
+            floatingactionbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(ProfileFragment.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+
+
+
+        return view;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        imgAva.setImageURI(uri);
+
     }
 }
